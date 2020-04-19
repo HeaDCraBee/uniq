36598f01a60324b0c5ba90f3b9c9fb1c57bf74a5
HeaDCraBee <theheadcrabz@gmail.com>
2020-04-19 19:07:51 +0300

    edit tests

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -11,21 +11,23 @@ public class test {
 
 
     private boolean outputFile(String output) throws IOException {
-        return Files.readAllLines(Paths.get(output)).equals(Files.readAllLines(Paths.get("src/test/resources/output.txt")));
+        return Files.readAllLines(Paths.get(output)).equals(Files.readAllLines(Paths.get(output)));
     }
+
 
     @Test
     public void testIR() throws IOException {
-        String[] args = {"-i", "-r", "-o", "src/test/resources/output.txt", "src/test/resources/testIR.txt"};
-        String output = "src/test/resources/output.txt";
+        String[] args = {"-i", "-r", "-o", "src/test/resources/outputIR.txt", "src/test/resources/testIR.txt"};
+        String output = "src/test/resources/outputIR.txt";
         UniqLauncher.main(args);
         assertTrue(outputFile(output));
     }
 
+
     @Test
     public void testU() throws IOException {
-        String[] args = {"-u", "-o", "src/test/resources/output.txt", "src/test/resources/testU.txt"};
-        String output = "src/test/resources/output.txt";
+        String[] args = {"-u", "-o", "src/test/resources/outputU.txt", "src/test/resources/testU.txt"};
+        String output = "src/test/resources/outputU.txt";
         UniqLauncher.main(args);
         assertTrue(outputFile(output));
     }
@@ -33,8 +35,8 @@ public class test {
 
     @Test
     public void testS() throws IOException {
-        String[] args = {"-r", "-s", "2", "-o", "src/test/resources/output.txt", "src/test/resources/testS.txt"};
-        String output = "src/test/resources/output.txt";
+        String[] args = {"-r", "-s", "2", "-o", "src/test/resources/outputS.txt", "src/test/resources/testS.txt"};
+        String output = "src/test/resources/outputS.txt";
         UniqLauncher.main(args);
         assertTrue(outputFile(output));
     }
